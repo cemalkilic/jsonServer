25f3949ec62c0cf5ef254699c088e176ca05fbad
Cemal Kılıç <cemalkilic96@gmail.com>
2021-01-01 12:36:34 +0300

    Sqlite implements the DataStore interface

--- database/sqlite.go
+++ database/sqlite.go
@@ -2,12 +2,18 @@ package database
 
 import (
     "database/sql"
+    "errors"
+    "github.com/cemalkilic/jsonServer/models"
     "log"
 
     _ "github.com/mattn/go-sqlite3"
 )
 
-var db *databaseStore
+type sqlDatabase struct {
+    db *sql.DB
+}
+
+var db DataStore
 
 func Init() {
 
@@ -16,9 +22,70 @@ func Init() {
         log.Fatal(err)
     }
 
-    db = NewDatabaseStore(sqliteDatabase)
+    db = &sqlDatabase{db: sqliteDatabase}
 }
 
-func GetDB() *databaseStore {
+func GetDB() DataStore {
     return db
 }
+
+func (s *sqlDatabase) Insert(endpoint models.CustomEndpoint) error {
+    insertEndpointSQL := `INSERT INTO endpoints(username, uri, content, statusCode) VALUES (?, ?, ?, ?)`
+    statement, err := s.db.Prepare(insertEndpointSQL)
+    if err != nil {
+        log.Fatal(err)
+        return err
+    }
+
+    _, err = statement.Exec(endpoint.Username, endpoint.URI, endpoint.Content, endpoint.StatusCode)
+    if err != nil {
+        return err
+    }
+
+    return nil
+}
+
+func (s *sqlDatabase) Select(username string, uri string) (models.CustomEndpoint, error) {
+    rows, err := s.db.Query("SELECT * FROM endpoints WHERE `username` = ? AND `uri` = ?", username, uri)
+    if err != nil {
+        log.Fatal(err)
+    }
+    defer rows.Close()
+
+    var idResp int
+    var usernameResp string
+    var uriResp string
+    var contentResp string
+    var statusCodeResp int
+
+    for rows.Next() { // Iterate and fetch the records from result cursor
+        rows.Scan(&idResp, &usernameResp, &uriResp, &contentResp, &statusCodeResp)
+    }
+    if err := rows.Err(); err != nil {
+        return models.CustomEndpoint{}, err
+    }
+
+    return models.CustomEndpoint{
+        ID:         idResp,
+        Username:   usernameResp,
+        URI:        uriResp,
+        Content:    contentResp,
+        StatusCode: statusCodeResp,
+    }, nil
+}
+
+func (s *sqlDatabase) SelectByID(id int) (models.CustomEndpoint, error) {
+    // TODO
+    return models.CustomEndpoint{}, errors.New("in SelectByID")
+}
+
+func (s *sqlDatabase) SelectAllByUser(username string) ([]models.CustomEndpoint, error) {
+    // TODO
+    return []models.CustomEndpoint{}, errors.New("in SelectAllByUser")
+}
+
+func (s *sqlDatabase) Delete(id int) error {
+    // TODO
+    return errors.New("in Delete")
+}
+
--- database/store.go
+++ database/store.go
@@ -1,10 +1,7 @@
 package database
 
 import (
-    "database/sql"
-    "errors"
     "github.com/cemalkilic/jsonServer/models"
-    "log"
 )
 
 type DataStore interface {
@@ -14,75 +11,3 @@ type DataStore interface {
     SelectAllByUser(username string) ([]models.CustomEndpoint, error)
     Delete(id int) error
 }
-
-type databaseStore struct {
-    db *sql.DB
-}
-
-func NewDatabaseStore(db *sql.DB) *databaseStore {
-    return &databaseStore{
-        db: db,
-    }
-}
-
-func (s *databaseStore) Insert(endpoint models.CustomEndpoint) error {
-    insertEndpointSQL := `INSERT INTO endpoints(username, uri, content, statusCode) VALUES (?, ?, ?, ?)`
-    statement, err := s.db.Prepare(insertEndpointSQL)
-    if err != nil {
-        log.Fatal(err)
-        return err
-    }
-
-    _, err = statement.Exec(endpoint.Username, endpoint.URI, endpoint.Content, endpoint.StatusCode)
-    if err != nil {
-        return err
-    }
-
-    return nil
-}
-
-func (s *databaseStore) Select(username string, uri string) (models.CustomEndpoint, error) {
-    rows, err := s.db.Query("SELECT * FROM endpoints WHERE `username` = ? AND `uri` = ?", username, uri)
-    if err != nil {
-        log.Fatal(err)
-    }
-    defer rows.Close()
-
-    var idResp int
-    var usernameResp string
-    var uriResp string
-    var contentResp string
-    var statusCodeResp int
-
-    for rows.Next() { // Iterate and fetch the records from result cursor
-        rows.Scan(&idResp, &usernameResp, &uriResp, &contentResp, &statusCodeResp)
-    }
-    if err := rows.Err(); err != nil {
-        return models.CustomEndpoint{}, err
-    }
-
-    return models.CustomEndpoint{
-        ID:         idResp,
-        Username:   usernameResp,
-        URI:        uriResp,
-        Content:    contentResp,
-        StatusCode: statusCodeResp,
-    }, nil
-}
-
-func (s *databaseStore) SelectByID(id int) (models.CustomEndpoint, error) {
-    // TODO
-    return models.CustomEndpoint{}, errors.New("in SelectByID")
-}
-
-func (s *databaseStore) SelectAllByUser(username string) ([]models.CustomEndpoint, error) {
-    // TODO
-    return []models.CustomEndpoint{}, errors.New("in SelectAllByUser")
-}
-
-func (s *databaseStore) Delete(id int) error {
-    // TODO
-    return errors.New("in Delete")
-}
-
-
